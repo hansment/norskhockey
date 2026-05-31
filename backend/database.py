868b51df30162b1e4f4ddeac7015e8b50b71e4f6
ChrisHansen <chr.hansen@live.no>
2026-05-31 21:00:25 +0200

    Endring av mail i OmOss kontakt

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "hockey.db")
+DB_PATH = os.environ.get("DB_PATH", os.path.join(BASE_DIR, "hockey.db"))
 
 def get_connection():
     """Åpner en tilkobling til databasen"""
